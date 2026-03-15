90817c2d4a710e97b1e59723ee1cc1b9e2ad97bb
NeX <falscherIdiot@gmail.com>
2026-03-15 17:59:36 +0100

    docs: added docs

--- request.php
+++ request.php
@@ -1,24 +1,62 @@
 <?php
 
+/**
+ * @author Fachpersonal
+ * @version 1.0
+ */
 class Request
 {
+    /**
+     * Allows to make a GET Request, by parsing the url and data (if needed).
+     *
+     * @param string $url API URL
+     * @param array $data Additional Data which are safed as POSTFIELDS, not always needed
+     * @return array response as JSON Object
+     */
     public static function get(string $url, array $data = []) : array
     {
         return self::call($url, Method::GET->name, $data);
     }
+    /**
+     * Allows to make a POST Request, by parsing the url and data (if needed).
+     *
+     * @param string $url API URL
+     * @param array $data Additional Data which are safed as POSTFIELDS, not always needed
+     * @return array response as JSON Object
+     */
     public static function post(string $url, array $data = []) : array
     {
         return self::call($url, Method::POST->name, $data);
     }
+    /**
+     * Allows to make a PUT Request, by parsing the url and data (if needed).
+     *
+     * @param string $url API URL
+     * @param array $data Additional Data which are safed as POSTFIELDS, not always needed
+     * @return array response as JSON Object
+     */
     public static function put(string $url, array $data = []) : array
     {
         return self::call($url, Method::PUT->name, $data);
     }
+    /**
+     * Allows to make a DELETE Request, by parsing the url and data (if needed).
+     *
+     * @param string $url API URL
+     * @param array $data Additional Data which are safed as POSTFIELDS, not always needed
+     * @return array response as JSON Object
+     */
     public static function delete(string $url, array $data = []) : array
     {
         return self::call($url, Method::DELETE->name, $data);
     }
 
+    /**
+     * @param string $url API url
+     * @param string $method Request method (GET/POST/PUT/DELETE)
+     * @param array $data Additional Data if needed
+     * @return array response as JSON Object
+     */
     private static function call(string $url, string $method, array $data) : array{
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
